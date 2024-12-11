00606ed0eaa27a68f862ad86c467e9ac9531857a
tahaamin2013 <tahaamindob2013@gmail.com>
2024-12-11 08:05:50 -0800

    Added Draggable false

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,18 @@ export default async function Home() {
         <Image
         src="/logo.svg"
         width={160}
+        draggable={false}
         height={160}
         alt="main image"
         priority
         quality={100}
-      />
+        />
         <div
           className="bg-cover bg-no-repeat w-full"
           style={{
             backgroundImage: `url(https://beautybugz.com/Gradient%20for%20comming%20soon.jpg)`,
           }}
-        >
+          >
           <h1 className="py-3 font-bold text-4xl sm:text-5xl bg-[#E5E5E5] mix-blend-lighten uppercase font-sans text-center">
             Coming Soon
           </h1>
@@ -31,14 +32,16 @@ export default async function Home() {
         width={1000}
         height={1000}
         alt="main image"
+        draggable={false}
         priority
         quality={100}
-      />
+        />
       <Image
         src="/Mobile Banner.svg"
         className="w-[90%] block sm:hidden mt-6" // Adjusted spacing
         width={1000}
         height={1000}
+        draggable={false}
         alt="main image"
         priority
         quality={100}
